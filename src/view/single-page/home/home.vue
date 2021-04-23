6fb98c3f27e925a3fbedcd4f425e92e60a1eef9a
Doublekk <467455219@qq.com>
2021-04-23 15:05:15 +0800

    update

--- src/view/single-page/home/home.vue
+++ src/view/single-page/home/home.vue
@@ -1,28 +1,46 @@
 <template>
   <div>
     <Row :gutter="20">
-      <i-col :xs="12" :md="8" :lg="4" v-for="(infor, i) in inforCardData" :key="`infor-${i}`" style="height: 120px;padding-bottom: 10px;">
-        <infor-card shadow :color="infor.color" :icon="infor.icon" :icon-size="36">
-          <count-to :end="infor.count" count-class="count-style"/>
+      <i-col :xs="12"
+             :md="8"
+             :lg="4"
+             v-for="(infor, i) in inforCardData"
+             :key="`infor-${i}`"
+             style="height: 120px;padding-bottom: 10px;">
+        <infor-card shadow
+                    :color="infor.color"
+                    :icon="infor.icon"
+                    :icon-size="36">
+          <count-to :end="infor.count"
+                    count-class="count-style" />
           <p>{{ infor.title }}</p>
         </infor-card>
       </i-col>
     </Row>
-    <Row :gutter="20" style="margin-top: 10px;">
-      <i-col :md="24" :lg="8" style="margin-bottom: 20px;">
+    <Row :gutter="20"
+         style="margin-top: 10px;">
+      <i-col :md="24"
+             :lg="8"
+             style="margin-bottom: 20px;">
         <Card shadow>
-          <chart-pie style="height: 300px;" :value="pieData" text="用户访问来源"></chart-pie>
+          <chart-pie style="height: 300px;"
+                     :value="pieData"
+                     text="用户访问来源"></chart-pie>
         </Card>
       </i-col>
-      <i-col :md="24" :lg="16" style="margin-bottom: 20px;">
+      <i-col :md="24"
+             :lg="16"
+             style="margin-bottom: 20px;">
         <Card shadow>
-          <chart-bar style="height: 300px;" :value="barData" text="每周用户活跃量"/>
+          <chart-bar style="height: 300px;"
+                     :value="barData"
+                     text="每周用户活跃量" />
         </Card>
       </i-col>
     </Row>
     <Row>
       <Card shadow>
-        <example style="height: 310px;"/>
+        <example style="height: 310px;" />
       </Card>
     </Row>
   </div>
@@ -77,7 +95,7 @@ export default {
 </script>
 
 <style lang="less">
-.count-style{
+.count-style {
   font-size: 50px;
 }
 </style>
